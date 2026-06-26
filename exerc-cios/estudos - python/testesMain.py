5bc48d9afa24894a0710726dd0673e1ddbb8360b
Diogo Bittar Pedro <bittard58@gmail.com>
2026-06-26 09:47:14 -0300

    CRUD - TAREFAS

--- exerc-cios/estudos - python/testesMain.py
+++ exerc-cios/estudos - python/testesMain.py
@@ -1,32 +1,26 @@
 from teste import *
 
 while True:
-    print("\n--- MENU ---\n")
-    print("1 - Adicionar tarefa")
-    print("2 - Listar tarefas")
-    print("3 - Atualizar uma tarefa")
-    print("4 - Remover tarefa")
-    print("5 - Sair")
+    print("\n\t---|MENU|---\n")
+    print("(1) - Adicionar uma tarefa")
+    print("(2) - Listar tarefas")
+    print("(3) - Atualizar uma tarefa")
+    print("(4) - Remover uma tarefa")
+    print("(5) - Sair do gerenciador de tarefas")
     print("-" * 30)
 
-    opcao = input("Escolha uma opção: ")
-
+    opcao = input("Escolha uma opção válida: ")
     match opcao:
         case "1":
             taskAdd()
-
         case "2":
             print(listTask())
-
         case "3":
             print(atualizarTask())
-
         case "4":
             removeTask()
-
         case "5":
-            print("Encerrando sistema...")
+            print("Até breve! :)\nSistema encerrando...")
             break
-
         case _:
-            print("Opção inválida.")
+            print("Opção inválida.\nDigite um dos número válidos (1 / 2 / 3 / 4 / 5)")
