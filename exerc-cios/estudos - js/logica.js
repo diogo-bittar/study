bf7903fd84c1a45b857d357aa1744746673a5a79
Diogo Bittar Pedro <bittard58@gmail.com>
2026-07-30 11:20:03 -0300

    Fix: Exs de lógica JS / PY

--- exerc-cios/estudos - js/logica.js
+++ exerc-cios/estudos - js/logica.js
@@ -8,32 +8,51 @@
 // alert(arrayInverse);
 
 // 2)
-let expressao = prompt("Digite a expressão matemática: ");
-let list = [];
-let eValida = true; 
+// let expressao = prompt("Digite a expressão matemática: ");
+// let list = [];
+// let eValida = true; 
 
-for(let i = 0; i < expressao.length; i++){
-    let caractere = expressao[i];
+// for(let i = 0; i < expressao.length; i++){
+//     let caractere = expressao[i];
 
-    if(caractere == "(" || caractere == "[" || caractere == "{"){
-        list.push(caractere);
-    } else if(caractere == ")" || caractere == "]" || caractere == "}"){
-        let topo = list.pop();
+//     if(caractere == "(" || caractere == "[" || caractere == "{"){
+//         list.push(caractere);
+//     } else if(caractere == ")" || caractere == "]" || caractere == "}"){
+//         let topo = list.pop();
 
-        if(
-            (caractere == ")" && topo != "(") ||
-            (caractere == "]" && topo != "[") ||
-            (caractere == "}" && topo != "{")
-        ){
-            eValida = false;
-            break;
-        }
-    }
-}
+//         if(
+//             (caractere == ")" && topo != "(") ||
+//             (caractere == "]" && topo != "[") ||
+//             (caractere == "}" && topo != "{")
+//         ){
+//             eValida = false;
+//             break;
+//         }
+//     }
+// }
 
 
-if(eValida && list.length === 0) {
-    alert("Expressão válida.");
-} else {
-    alert("Expressão inválida.");
-}
+// if(eValida && list.length === 0) {
+//     alert("Expressão válida.");
+// } else {
+//     alert("Expressão inválida.");
+// }
+
+
+// 4)
+// const isPallindrome = prompt("Informe a palavra: ");
+// const result = isPallindrome.replace(/\s+/g, '').toLowerCase();
+
+// let inverse = "";
+
+// for(let i = result.length - 1; i >= 0; i--){
+//     inverse += result[i]
+// }
+
+// if(result === inverse){
+//     alert("É um palíndromo!");
+// }else {
+//     alert("Não é um palíndromo!");
+// }
+
+// 5)
--- exerc-cios/estudos - python/logica.py
+++ exerc-cios/estudos - python/logica.py
@@ -1,19 +1,40 @@
+# matriz = [
+#     [1, 5, 9],
+#     [2, 6, 10],
+#     [3, 7, 11]
+# ]
+
+# encontrado = False
+# number = int(input("Digite o número que deseja buscar no tabuleiro: "))
+
+# for i in range(len(matriz)):
+#     for j in range(len(matriz[i])):
+#         if matriz[i][j] == number:
+#             encontrado = True
+#             print(f'\nO número encontrado na Posição: Linha {i}, Coluna {j}.')
+#             break
+
+# if encontrado == False:
+#     print(f'\nO número {number} está fora da matriz...\n'
+#           f'\n\tA matriz original:\n {matriz}')
+
+
+# Exs 2)
+
 matriz = [
-    [1, 5, 9],
-    [2, 6, 10],
-    [3, 7, 11]
+    #0  1   2
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ]
 
-encontrado = False
-number = int(input("Digite o número que deseja buscar no tabuleiro: "))
+m = len(matriz)
 
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        if matriz[i][j] == number:
-            encontrado = True
-            print(f'\nO número encontrado na Posição: Linha {i}, Coluna {j}.')
-            break
+matrizNova = [[0] * m for _ in range(m)]
 
-if encontrado == False:
-    print(f'\nO número {number} está fora da matriz...\n'
-          f'\n\tA matriz original:\n {matriz}')
+for i in range(m):
+    for j in range(m):
+        matrizNova[j][m - 1 - i]  = matriz[i][j]
+
+for linha in matrizNova:
+    print(linha)
